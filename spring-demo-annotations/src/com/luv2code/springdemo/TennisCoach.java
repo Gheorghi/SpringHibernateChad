55195e07f20b1d637f06de4d2b9393839df47ac4
c-gheouzun <c-gheorghi.uzun@edifecs.com>
2019-11-12 19:27:42 +0200

    Autowiring. Field injection. Automatically injects

--- spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -6,18 +6,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
+	@Autowired
 	private FortuneService fortuneService;
 	
 	public TennisCoach() {
 		System.out.println(">> TennisCoach: inside default constructor");
 	}
 
-	@Autowired
-	public void doSomeCrazyStaff(FortuneService theFortuneService) {
-		fortuneService = theFortuneService;
-		System.out.println(">> TennisCoach: inside doSomeCrazyStaff");
-	}
-
+	/*
+	 * @Autowired public void doSomeCrazyStaff(FortuneService theFortuneService) {
+	 * fortuneService = theFortuneService;
+	 * System.out.println(">> TennisCoach: inside doSomeCrazyStaff"); }
+	 */
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your backhand volley";
