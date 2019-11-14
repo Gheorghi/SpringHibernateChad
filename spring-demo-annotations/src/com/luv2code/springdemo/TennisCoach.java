e2883cb10aef3633e6f6d7c65744565a6608a59c
c-gheouzun <c-gheorghi.uzun@edifecs.com>
2019-11-14 19:56:54 +0200

    @PostConstruct & @PreDestroy. void return type is more preferable since here we can't handle an return value. For 'PROTOTYPE' beans Spring doesn't call the @PreDestroy method

--- spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -1,20 +1,35 @@
 package com.luv2code.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Component
-@Scope("prototype")
 public class TennisCoach implements Coach {
 
 	@Autowired
 	@Qualifier("basicFortuneService")
 	private FortuneService fortuneService;
-	
+
 	public TennisCoach() {
 		System.out.println(">> TennisCoach: inside default constructor");
+	}
+
+	// define my init method
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyStartupStuff");
+	}
+
+	// define my init method
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyCleanupStuff");
 	}
 
 	/*
