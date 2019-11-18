d809a2296bba5a1dfa1416d32d227b95db568a5c
c-gheouzun <c-gheorghi.uzun@edifecs.com>
2019-11-18 18:11:20 +0200

    Practice #7 IoC and DI with Java Configuration

--- spring-demo-annotations/src/com/luv2code/springdemo/HappyFortuneService.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/HappyFortuneService.java
@@ -7,7 +7,6 @@ public class HappyFortuneService implements FortuneService{
 
 	@Override
 	public String getFortune() {
-		// TODO Auto-generated method stub
 		return "Today is your lucky day!";
 	}
 
