da8a5c4a941ae89665994f9dd6b05f1b8a5b9611
c-gheouzun <c-gheorghi.uzun@edifecs.com>
2019-12-03 17:09:19 +0200

    Continue with add Data to Form in MVC form

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -1,7 +1,11 @@
 package com.luv2code.springdemo.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import javax.servlet.*;
+
+import com.sun.net.httpserver.HttpServer;
 
 @Controller
 public class HelloWorldController {
@@ -15,6 +19,26 @@ public class HelloWorldController {
 	//need a controller method to process the HTML form
 	@RequestMapping("/processForm")
 	public String processForm() {
+		return "helloworld";
+	}
+	
+	// new a controller method to read form data and
+	// add data to the model
+	
+	@RequestMapping("/processFormVertionTwo")
+	public String letsShoutDude(HttpServletRequest request, Model model) {
+		// read the request parametr from the HTML form
+		String theName = request.getParametr("studentName");
+		
+		//convert the data to all caps
+		theName = theName.toUpperCase();
+		
+		// create the message
+		String result = "Yo" + theName;
+		
+		//add message to the model
+		model.addAttribute("message", result);
+		
 		return "helloworld";
 	}
 
