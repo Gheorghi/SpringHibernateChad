c1bd76355dd5e0399bfe09b1bb6519c5410db23b
c-gheouzun <c-gheorghi.uzun@edifecs.com>
2020-01-29 12:49:14 +0200

    Delete from DB. also Delete using HQL

--- hibernate-tutorial/src/com/luv2code/hibernate/demo/DeleteStudentDemo.java
+++ hibernate-tutorial/src/com/luv2code/hibernate/demo/DeleteStudentDemo.java
@@ -6,7 +6,7 @@ import org.hibernate.cfg.Configuration;
 
 import com.luv2code.hibernate.demo.entity.Student;
 
-public class UpdateStudentDemo {
+public class DeleteStudentDemo {
 
 	public static void main(String[] args) {
 
@@ -32,8 +32,12 @@ public class UpdateStudentDemo {
 			Student myStudent = session.get(Student.class, studentId);
 			
 			//delete the student
-			System.out.println("Deleting student: " + myStudent);
-			session.delete(myStudent);
+//			System.out.println("Deleting student: " + myStudent);
+//			session.delete(myStudent);
+			
+			//delete student id=2
+			System.out.println("Deleting student id=2");
+			session.createQuery("delete from Student where id=2").executeUpdate();
 
 			//commit the transaction
 			session.getTransaction().commit();
