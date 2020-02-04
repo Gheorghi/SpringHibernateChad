5b5ba6c5f19de14e1015b2804c8141584dc08a29
c-gheouzun <c-gheorghi.uzun@edifecs.com>
2020-02-04 15:18:25 +0200

    CreateInstructorDemo.class add course details && and add to the DB

--- hb-03-one-to-many/src/com/luv2code/hibernate/demo/CreateCoursesDemo.java
+++ hb-03-one-to-many/src/com/luv2code/hibernate/demo/CreateCoursesDemo.java
@@ -9,7 +9,7 @@ import com.luv2code.hibernate.demo.entity.Instructor;
 import com.luv2code.hibernate.demo.entity.InstructorDetail;
 import com.luv2code.hibernate.demo.entity.Student;
 
-public class CreateInstructorDemo {
+public class CreateCoursesDemo {
 
 	public static void main(String[] args) {
 
@@ -25,24 +25,24 @@ public class CreateInstructorDemo {
 		Session session = factory.getCurrentSession();
 		
 		try {		
+			// create the objects
+			Instructor tempInstructor = new Instructor("Susan", "public", "susan.public@luv2code.com"); 
+
+			InstructorDetail tempInstructorDetail = new InstructorDetail("http://www.youtube","Video Games");
+
+			// associate the objects
+			tempInstructor.setInstructorDetail(tempInstructorDetail);
+
 			// start a transaction
 			session.beginTransaction();
 
-			//get the instructor from db
-			int theId = 1;
-			Instructor tempInstructor = session.get(Instructor.class, theId);
-			
-			//create some courses
-			Course tempCourse1 = new Course("Air Guitar -The Ultimate Guide");
-			Course tempCourse2 = new Course("The Pintball Masterclass");
-			
-			//add courses to instructor
-			tempInstructor.add(tempCourse1);
-			tempInstructor.add(tempCourse2);
-			
-			//save the courses
-			session.save(tempCourse1);
-			session.save(tempCourse2);
+			// save the instructor
+			//
+			// Note: this will ALSO save the details object
+			// because of CascadeType.ALL
+			//
+			System.out.println("Saving instructor: "+tempInstructor);
+			session.save(tempInstructor);
 
 			// commit transaction
 			session.getTransaction().commit();
