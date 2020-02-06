ce22a978abb659ac1503a5fb2d93a0bd8190f778
c-gheouzun <c-gheorghi.uzun@edifecs.com>
2020-02-06 10:44:18 +0200

    Course & Student. Setup relationships. @JointTable

--- hb-05-many-to-many/src/com/luv2code/hibernate/demo/entity/Student.java
+++ hb-05-many-to-many/src/com/luv2code/hibernate/demo/entity/Student.java
@@ -1,10 +1,17 @@
 package com.luv2code.hibernate.demo.entity;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -22,6 +29,15 @@ public class Student {
 	private String lastName;
 	@Column(name = "email")
 	private String email;
+	
+	@ManyToOne(
+			fetch=FetchType.LAZY,
+			cascade= {CascadeType.PERSIST, CascadeType.DETACH, 
+			CascadeType.MERGE, CascadeType.REFRESH})
+	@JoinTable(name="course_student",
+	joinColumns=@JoinColumn(name="student_id"),
+	inverseJoinColumns = @JoinColumn(name="course_id"))
+	private List<Course> courses;
 
 	public Student() {
 	}
@@ -63,6 +79,15 @@ public class Student {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+	
 
 	@Override
 	public String toString() {
