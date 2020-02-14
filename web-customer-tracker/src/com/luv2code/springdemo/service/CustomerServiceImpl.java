9db23e8bc65d478fa59f8aa9a2f00a5085a69d7e
c-gheouzun <c-gheorghi.uzun@edifecs.com>
2020-02-14 18:36:23 +0200

    DELTE Customer implementation

--- web-customer-tracker/src/com/luv2code/springdemo/service/CustomerServiceImpl.java
+++ web-customer-tracker/src/com/luv2code/springdemo/service/CustomerServiceImpl.java
@@ -39,4 +39,11 @@ public class CustomerServiceImpl implements CustomerService {
 	}
 
 
+	@Override
+	@Transactional
+	public void deleteCustomer(int theId) {
+		customerDAO.deleteCustomer(theId);
+	}
+
+
 }
