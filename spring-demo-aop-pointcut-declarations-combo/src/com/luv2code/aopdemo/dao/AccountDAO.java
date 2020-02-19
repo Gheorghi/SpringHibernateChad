8232c814d53e2a597d072077b838f18e85add87d
c-gheouzun <c-gheorghi.uzun@edifecs.com>
2020-02-19 16:32:49 +0200

    Add properties to AccountDAO, add setters and getters

--- spring-demo-aop-pointcut-declarations-combo/src/com/luv2code/aopdemo/dao/AccountDAO.java
+++ spring-demo-aop-pointcut-declarations-combo/src/com/luv2code/aopdemo/dao/AccountDAO.java
@@ -6,6 +6,9 @@ import com.luv2code.aopdemo.Account;
 
 @Component
 public class AccountDAO {
+	private String name;
+	private String serviceCode;
+	
 	
 	public void addAccount(Account theAccount, boolean vipFlag) {
 		
@@ -18,4 +21,25 @@ public class AccountDAO {
 		return true;
 	}
 
+	public String getName() {
+		System.out.println(getClass() + ": in getName()");
+		return name;
+	}
+
+	public void setName(String name) {
+		System.out.println(getClass() + ": in setName(String name)");
+		this.name = name;
+	}
+
+	public String getServiceCode() {
+		System.out.println(getClass() + ": in getServiceCode()");
+		return serviceCode;
+	}
+
+	public void setServiceCode(String serviceCode) {
+		System.out.println(getClass() + ": in setServiceCode(String serviceCode)");
+		this.serviceCode = serviceCode;
+	}
+
+	
 }
