2884d0dc95710696734e80acb62aca494587d7e0
c-gheouzun <c-gheorghi.uzun@edifecs.com>
2020-02-20 19:32:05 +0200

    Implement @AfterReturning advice

--- spring-demo-aop-z-after-returning/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop-z-after-returning/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,5 +1,7 @@
 package com.luv2code.aopdemo.aspect;
 
+import java.util.List;
+
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
@@ -15,36 +17,49 @@ import com.luv2code.aopdemo.Account;
 @Order(2)
 public class MyDemoLoggingAspect {
 
+	// add a new advice for @AfterReturning on the findAccounts method
+
+	@AfterReturning(pointcut = "execution(* com.luv2code.aopdemo.dao.AccountDAO.findAccounts(..))", returning = "result")
+	public void afterReturningFindAccountsAdvice(JoinPoint theJoinPoint, List<Account> result) {
+
+		// print out which method we are advising on
+		String method = theJoinPoint.getSignature().toShortString();
+		System.out.println("\n=====>>> Executing @AfterRetuning on mehtod" + method);
+
+		// print out the results of the method call
+		System.out.println("\n====>>> result is: " + result);
+	}
+
 	@Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
 	public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
-		
-		System.out.println("\n=====>>> Executing @Before advice on method");	
-		
+
+		System.out.println("\n=====>>> Executing @Before advice on method");
+
 		// display the method signature
 		MethodSignature methodSig = (MethodSignature) theJoinPoint.getSignature();
-		
+
 		System.out.println("Method: " + methodSig);
-		
+
 		// display method arguments
-		
+
 		// get args
 		Object[] args = theJoinPoint.getArgs();
-		
+
 		// loop thru args
 		for (Object tempArg : args) {
 			System.out.println(tempArg);
-			
+
 			if (tempArg instanceof Account) {
-				
+
 				// downcast and print Account specific stuff
 				Account theAccount = (Account) tempArg;
-				
+
 				System.out.println("account name: " + theAccount.getName());
-				System.out.println("account level: " + theAccount.getLevel());								
+				System.out.println("account level: " + theAccount.getLevel());
 
 			}
 		}
-		
+
 	}
 
 }
