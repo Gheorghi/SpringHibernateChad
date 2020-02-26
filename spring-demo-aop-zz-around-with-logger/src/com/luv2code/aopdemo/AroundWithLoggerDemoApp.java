031ec8a4526c945ce3e1673aa5518fb2bfc6c834
c-gheouzun <c-gheorghi.uzun@edifecs.com>
2020-02-26 18:00:44 +0200

    Convert System.out.println into logs

--- spring-demo-aop-zz-around-with-logger/src/com/luv2code/aopdemo/AroundWithLoggerDemoApp.java
+++ spring-demo-aop-zz-around-with-logger/src/com/luv2code/aopdemo/AroundWithLoggerDemoApp.java
@@ -1,11 +1,15 @@
 package com.luv2code.aopdemo;
 
+import java.util.logging.Logger;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.luv2code.aopdemo.service.TrafficFortuneService;
 
 public class AroundWithLoggerDemoApp {
-
+	
+	private static Logger myLogger = Logger.getLogger(AroundWithLoggerDemoApp.class.getName());
+	
 	public static void main(String[] args) {
 
 		// read spring config java class
@@ -14,15 +18,15 @@ public class AroundWithLoggerDemoApp {
 		// get the bean from spring container
 		TrafficFortuneService theFortuneService = context.getBean("trafficFortuneService", TrafficFortuneService.class);
 
-		System.out.println("\nMain Program: AroundDemoApp");
+		myLogger.info("\nMain Program: AroundDemoApp");
 		
-		System.out.println("Calling getFortune");
+		myLogger.info("Calling getFortune");
 		
 		String data = theFortuneService.getFortune();
 		
-		System.out.println("\nMy Fortune is: "+data);
+		myLogger.info("\nMy Fortune is: "+data);
 		
-		System.out.println("Finished");
+		myLogger.info("Finished");
 		
 		// close the context
 		context.close();
--- spring-demo-aop-zz-around-with-logger/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop-zz-around-with-logger/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,6 +1,7 @@
 package com.luv2code.aopdemo.aspect;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -20,13 +21,15 @@ import com.luv2code.aopdemo.Account;
 @Component
 @Order(2)
 public class MyDemoLoggingAspect {
+	
+	private Logger myLogger = Logger.getLogger(getClass().getName());
 
 	@Around("execution(* com.luv2code.aopdemo.service.*.getFortune(..))")
 	public Object aroundGetFortune(ProceedingJoinPoint theProceedingJoinPoint) throws Throwable {
 
 		// print out method we are advising on
 		String method = theProceedingJoinPoint.getSignature().toShortString();
-		System.out.println("\n=====>>> Executing @Around on mehtod" + method);
+		myLogger.info("\n=====>>> Executing @Around on mehtod" + method);
 
 		// get begin timestamp
 		long begin = System.currentTimeMillis();
@@ -39,7 +42,7 @@ public class MyDemoLoggingAspect {
 
 		// compute duration and display it
 		long duration = end - begin;
-		System.out.println("\n=====> Duration: "+ duration / 1000.0 + " seconds");
+		myLogger.info("\n=====> Duration: "+ duration / 1000.0 + " seconds");
 		
 		return result;
 	}
@@ -48,7 +51,7 @@ public class MyDemoLoggingAspect {
 	public void afterFinallyFindAccountsAdvice(JoinPoint theJoinPoint) {
 		// print out which method we are advising on
 		String method = theJoinPoint.getSignature().toShortString();
-		System.out.println("\n=====>>> Executing @After (finally) on mehtod" + method);
+		myLogger.info("\n=====>>> Executing @After (finally) on mehtod" + method);
 
 	}
 
@@ -56,10 +59,10 @@ public class MyDemoLoggingAspect {
 	public void afterThrowingFindAccountsAdvice(JoinPoint theJoinPoint, Throwable theExc) {
 		// print out which method we are advising on
 		String method = theJoinPoint.getSignature().toShortString();
-		System.out.println("\n=====>>> Executing @AfterThrowing on mehtod" + method);
+		myLogger.info("\n=====>>> Executing @AfterThrowing on mehtod" + method);
 
 		// log the exception
-		System.out.println("\n=====>>> The exception is: " + theExc);
+		myLogger.info("\n=====>>> The exception is: " + theExc);
 
 	}
 
@@ -68,17 +71,17 @@ public class MyDemoLoggingAspect {
 
 		// print out which method we are advising on
 		String method = theJoinPoint.getSignature().toShortString();
-		System.out.println("\n=====>>> Executing @AfterRetuning on mehtod" + method);
+		myLogger.info("\n=====>>> Executing @AfterRetuning on mehtod" + method);
 
 		// print out the results of the method call
-		System.out.println("\n====>>> result is: " + result);
+		myLogger.info("\n====>>> result is: " + result);
 
 		// let's post-process the data ... let's modify it :-)
 
 		// convert the account names to uppercase
 		convertAccountNamesToUpperCase(result);
 
-		System.out.println("\n====>>> result is: " + result);
+		myLogger.info("\n====>>> result is: " + result);
 	}
 
 	private void convertAccountNamesToUpperCase(List<Account> result) {
@@ -97,12 +100,12 @@ public class MyDemoLoggingAspect {
 	@Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
 	public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
 
-		System.out.println("\n=====>>> Executing @Before advice on method");
+		myLogger.info("\n=====>>> Executing @Before advice on method");
 
 		// display the method signature
 		MethodSignature methodSig = (MethodSignature) theJoinPoint.getSignature();
 
-		System.out.println("Method: " + methodSig);
+		myLogger.info("Method: " + methodSig);
 
 		// display method arguments
 
@@ -111,15 +114,15 @@ public class MyDemoLoggingAspect {
 
 		// loop thru args
 		for (Object tempArg : args) {
-			System.out.println(tempArg);
+			myLogger.info(tempArg.toString());
 
 			if (tempArg instanceof Account) {
 
 				// downcast and print Account specific stuff
 				Account theAccount = (Account) tempArg;
 
-				System.out.println("account name: " + theAccount.getName());
-				System.out.println("account level: " + theAccount.getLevel());
+				myLogger.info("account name: " + theAccount.getName());
+				myLogger.info("account level: " + theAccount.getLevel());
 
 			}
 		}
