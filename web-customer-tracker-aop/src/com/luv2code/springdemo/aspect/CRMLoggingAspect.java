d213440880ae10b02d6b959a3c5c48f22f4b8237
c-gheouzun <c-gheorghi.uzun@edifecs.com>
2020-02-27 11:52:42 +0200

    Done with adding Logging with AOP.

--- web-customer-tracker-aop/src/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
+++ web-customer-tracker-aop/src/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
@@ -3,6 +3,7 @@ package com.luv2code.springdemo.aspect;
 import java.util.logging.Logger;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -23,7 +24,7 @@ public class CRMLoggingAspect {
 	}
 	
 	// do the same for service and DAO
-	@Pointcut("execution(* com.luv2code.springdemo.controller.*.*(..))")
+	@Pointcut("execution(* com.luv2code.springdemo.service.*.*(..))")
 	private void forServicePackage() {	}
 	
 	@Pointcut("execution(* com.luv2code.springdemo.dao.*.*(..))")
@@ -52,6 +53,23 @@ public class CRMLoggingAspect {
 		}
 	}
 	
+	
+	
 	// add @AfterReturning advice
-
+	@AfterReturning(
+			pointcut="forAppFlow()",
+			returning="theResult")
+	public void afterReturning(JoinPoint theJoinPoint, Object theResult) {
+		
+		// display method we are returning from
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("====>> in @AfterReturning: from method: " + theMethod);
+		
+		// display data returned
+		myLogger.info("=====>> result: " + theResult);
+		
+	}
+	
+	
+	
 }
