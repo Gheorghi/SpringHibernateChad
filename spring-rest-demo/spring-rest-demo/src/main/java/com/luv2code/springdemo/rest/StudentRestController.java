8416c55ef156fb25c390aeaa76a6c6783aae698d
Gheorghi <uzungh@gmail.com>
2020-07-28 07:54:10 +0300

    a. Add javax dependency. b. Impliment gettting an user by @PathVariable

--- spring-rest-demo/spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,16 +16,35 @@ import com.luv2code.springdemo.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 	
+	private List<Student> theStudents;
+	
+	//define @PostConstruct to load the students data ... only once!
+	
+	@PostConstruct
+	public void loadData() {
+		theStudents = new ArrayList<Student>();
+		
+		theStudents.add(new Student("Poorima", "Patel"));
+		theStudents.add(new Student("Mario", "Rossi"));
+		theStudents.add(new Student("Mary", "Smith"));
+	}
+	
 	//define endpoint for "/students" - return list of students
 	
 	@GetMapping("/students")
 	public List<Student> getStudents(){
 		
-		List<Student> theStudents = new ArrayList<>();
-		theStudents.add(new Student("Poorima", "Patel"));
-		theStudents.add(new Student("Mario", "Rossi"));
-		theStudents.add(new Student("Mary", "Smith"));
 		return theStudents;
+	}
+	
+	//define endpoint for "/students/{studentId}" - return student at index
+	
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		
+		// just index into the list ... keep it simple for now
+		
+		return theStudents.get(studentId);
 	}
 
 }
